f897416654a5f3cb7d8c089e338d01cb188e362f
akilamaithri <akilamaithripala@gmail.com>
2024-03-01 23:08:19 +0530

    add carousel and title style change

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,68 @@ menuTrigger.onclick = function() {
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
 }
+
+var index = 0;
+var locations = ["Amsterdam, The Netherlands", "Sydney, Australia", "San Francisco, California"];
+
+var slides = document.getElementsByClassName("slides");
+var nextArrow = document.getElementById("next");
+var previousArrow = document.getElementById("previous");
+var place = document.getElementById("place");
+var dotsContainer = document.getElementById("dotsContainer");
+var dotArray = document.getElementsByClassName("dots");
+
+createDots();
+showSlides(index);
+
+function createDots() {
+    for (var i = 0; i < slides.length; i++) {
+        var dot = document.createElement("span");
+        dot.className = "dots";
+        dotsContainer.appendChild(dot);
+    }
+}
+
+function showSlides(x) {
+    if (x > slides.length - 1) {
+        index = 0;
+    }
+    if (x < 0) {
+        index = slides.length - 1;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dotArray[i].className = "dots";
+    }
+
+    slides[index].style.display = "block";
+    dotArray[index].className += " activeDot";
+    place.innerHTML = locations[index];
+}
+
+nextArrow.onclick = function () {
+    index += 1;
+    showSlides(index);
+};
+
+previousArrow.onclick = function () {
+    index -= 1;
+    showSlides(index);
+};
+
+function autoSlide() {
+    index += 1;
+    showSlides(index);
+}
+
+var slideInterval = setInterval(autoSlide, 4000);
+
+// Pause the slideshow when mouse hovers over the slides
+for (var i = 0; i < slides.length; i++) {
+    slides[i].addEventListener("mouseenter", function () {
+        clearInterval(slideInterval);
+    });
+    slides[i].addEventListener("mouseleave", function () {
+        slideInterval = setInterval(autoSlide, 4000);
+    });
+}
--- index.md
+++ index.md
@@ -1,5 +1,5 @@
 ---
-title: Jekyll Serif Theme
+title: Research Lab
 layout: home
 description: Jekyll Serif contains content types for a typical business website. The theme is fully responsive, blazing fast and artfully illustrated.
 intro_image: "images/illustrations/pointing.svg"
@@ -8,6 +8,8 @@ intro_image_hide_on_mobile: true
 show_call_box: true
 ---
 
-# Serif - A Jekyll Business Theme.
+Welcome to
 
-Serif contains content types for a typical business website. The theme is fully responsive, blazing fast and artfully illustrated.
+# Research Lab
+
+ESRG at Stanford CS is like your go-to crew for crushing Internet security, owning privacy problems, and shutting down online hate and fake news. We're not your average lab – we build slick systems, gather global data, and cook up dope solutions. Ready to vibe with us and change the digital game? 
